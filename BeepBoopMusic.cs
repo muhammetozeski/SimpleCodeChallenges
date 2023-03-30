e51c5546b9d2fcd4234e48544058eac9da19b695
muhammet484 <51961772+muhammet484@users.noreply.github.com>
2023-03-31 02:58:05 +0300

    Create BeepBoopMusic.cs

---
+++ BeepBoopMusic.cs
@@ -0,0 +1,72 @@
+
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Beep(600, 600);
+        Console.Beep(500, 200);
+        Console.Beep(500, 400);
+        Console.Beep(400, 200);
+        Console.Beep(500, 200);
+        Console.Beep(400, 400);
+        Console.Beep(400, 200);
+        Console.Beep(500, 200);
+        Console.Beep(500, 200);
+        Console.Beep(600, 600);
+        Console.Beep(400, 200);
+        Console.Beep(500, 200);
+        Console.Beep(400, 400);
+
+        Console.Beep(600, 200);
+        Console.Beep(500, 200);
+        Console.Beep(400, 200);
+        Console.Beep(500, 400);
+        Console.Beep(400, 200);
+        Console.Beep(500, 200);
+        Console.Beep(400, 200);
+        Console.Beep(450, 600);
+        Console.Beep(600, 200);
+        Console.Beep(500, 200);
+        Console.Beep(400, 600);
+
+
+        Console.Beep(600, 200);
+        Console.Beep(500, 200);
+        Console.Beep(450, 400);
+        Console.Beep(500, 200);
+        Console.Beep(500, 200);
+        Console.Beep(450, 200);
+        Console.Beep(450, 200);
+        Console.Beep(500, 400);
+        Console.Beep(400, 400);
+        Console.Beep(500, 200);
+        Console.Beep(400, 400);
+        Console.Beep(500, 200);
+        Console.Beep(400, 200);
+        Console.Beep(300, 600);
+
+        Console.Beep(300, 200);
+        Console.Beep(400, 400);
+        Console.Beep(300, 200);
+        Console.Beep(400, 200);
+        Console.Beep(300, 200);
+        Console.Beep(400, 400);
+        Console.Beep(300, 200);
+        Console.Beep(400, 200);
+        Console.Beep(500, 600);
+        Console.Beep(450, 200);
+        Console.Beep(400, 600);
+
+
+        Console.Beep(350, 200);
+        Console.Beep(500, 600);
+        Console.Beep(400, 200);
+        Console.Beep(400, 400);
+        Console.Beep(500, 200);
+        Console.Beep(400, 200);
+        Console.Beep(300, 600);
+    }
+}
